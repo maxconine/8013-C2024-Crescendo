61b37437cb37a6e4c571a6f22fabcf7d08dad207
maxconine <58146795+maxconine@users.noreply.github.com>
2024-04-02 23:59:15 -0400

    wait for aquisition

--- src/main/java/com/team8013/frc2024/auto/actions/WaitForAquisition.java
+++ src/main/java/com/team8013/frc2024/auto/actions/WaitForAquisition.java
@@ -1,33 +1,31 @@
 package com.team8013.frc2024.auto.actions;
-// package com.team8013.frc2023.auto.actions;
 
-// import com.team8013.frc2023.subsystems.Drive;
-// import com.team8013.frc2023.subsystems.EndEffector;
+import com.team8013.frc2024.subsystems.EndEffectorREV;
 
-// public class WaitForAquisition implements Action {
-//     EndEffector effector;
+public class WaitForAquisition implements Action {
+    EndEffectorREV effector;
 
-//     public WaitForAquisition() {
-//         effector = EndEffector.getInstance();
-//     }
+    public WaitForAquisition() {
+        effector = EndEffectorREV.getInstance();
+    }
 
-//     @Override
-//     public boolean isFinished() {
-//         return effector.hasGamePiece();
-//     }
+    @Override
+    public boolean isFinished() {
+        return effector.hasGamePiece();
+    }
 
-//     @Override
-//     public void start() {
-//     }
+    @Override
+    public void start() {
+    }
 
-//     @Override
-//     public void update() {
+    @Override
+    public void update() {
 
-//     }
+    }
 
-//     @Override
-//     public void done() {
+    @Override
+    public void done() {
 
-//     }
+    }
 
-// }
+}
